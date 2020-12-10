a5db411ae10d8ab693669e84cd7e22ec58235db7
Joshua Jungen <joshua.jungen@soptim.de>
2020-12-10 15:53:57 +0100

    implement submit with recalc

--- src/main/java/de/ccd/groupprio/domain/submission/SubmissionService.java
+++ src/main/java/de/ccd/groupprio/domain/submission/SubmissionService.java
@@ -1,16 +1,25 @@
 package de.ccd.groupprio.domain.submission;
 
+import de.ccd.groupprio.domain.prioritization.PrioritizationService;
+
 import java.util.List;
 
 public class SubmissionService {
 
+    private final PrioritizationService prioritizationService;
     private final SubmissionRepository submissionRepository;
 
-    public SubmissionService(SubmissionRepository submissionRepository) {
+    public SubmissionService(PrioritizationService prioritizationService, SubmissionRepository submissionRepository) {
+        this.prioritizationService = prioritizationService;
         this.submissionRepository = submissionRepository;
     }
 
-    public void submit(long projectId, List<PrioItem> items) {
+    public void submitWithRecalc(long projectId, List<PrioItem> items) {
+        this.submit(projectId, items);
+        prioritizationService.calcPriorities(projectId);
+    }
+
+    private void submit(long projectId, List<PrioItem> items) {
         Submission submission = new Submission(items);
         submissionRepository.save(projectId, submission);
     }
