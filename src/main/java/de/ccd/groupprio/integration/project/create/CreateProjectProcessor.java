4cdf8a06e3ccb144bd89bd9df13c4f946f544583
Joshua Jungen <joshua.jungen@soptim.de>
2021-04-29 16:27:39 +0200

    fix unique project title constraint

--- src/main/java/de/ccd/groupprio/integration/project/create/CreateProjectProcessor.java
+++ src/main/java/de/ccd/groupprio/integration/project/create/CreateProjectProcessor.java
@@ -1,6 +1,7 @@
 package de.ccd.groupprio.integration.project.create;
 
 import de.ccd.groupprio.domain.data.ProjectAggregate;
+import de.ccd.groupprio.domain.event.ProjectCreatedEvent;
 import de.ccd.groupprio.event_store.EventStore;
 import lombok.RequiredArgsConstructor;
 
@@ -12,11 +13,20 @@ class CreateProjectProcessor {
     private final EventStore eventStore;
 
     public CreatedProjectResponse process(CreateProjectCommand cmd) {
-        var project = ProjectAggregate.create(UUID.randomUUID().toString(), cmd.title, cmd.createdByClientId, cmd.multiSubmission, cmd.items);
+        assertUniqueProjectTitle(cmd);
 
-        // TODO check Title is unique for user
+        var project = ProjectAggregate.create(UUID.randomUUID().toString(), cmd.title, cmd.createdByClientId, cmd.multiSubmission, cmd.items);
 
         eventStore.record(project.getChanges());
         return new CreatedProjectResponse(project.getId());
+    }
+
+    private void assertUniqueProjectTitle(CreateProjectCommand cmd) {
+        var titleAlreadyExists = eventStore.replay(ProjectCreatedEvent.class)
+                                           .filter(e -> e.getOwnerId().equals(cmd.createdByClientId))
+                                           .anyMatch(e -> e.getTitle().equals(cmd.title));
+        if (titleAlreadyExists) {
+            throw new IllegalArgumentException("Title must be unique.");
+        }
     }
 }
